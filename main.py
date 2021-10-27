2413506c9c1799fad00355c0503ddaedecb559fe
singularity <12184989+singularity-s0@users.noreply.github.com>
2021-10-27 17:53:43 +0800

    multiple user support

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 import json
 import random
 import time
-import os
 from threading import Thread
 
 import easyocr
 import requests
 from bs4 import BeautifulSoup
 
-# Constants
-usernames = os.getenv("USERNAMES")
-passwords = os.getenv("PASSWORDS")
+USERS = [('username', 'password', 36000)]
+
 CUDA = None
-reader = easyocr.Reader(['en'], gpu=False)
 headers = {
     "Origin": "https://zlapp.fudan.edu.cn",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) "
-                  "Version/14.0 Mobile/15E148 Safari/604.1",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
     "Referer": "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily?from=history",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-us",
     "Connection": "keep-alive"
 }
-login_url = "https://uis.fudan.edu.cn/authserver/login?service=https%3A%2F%2Fzlapp.fudan.edu.cn%2Fa_fudanzlapp%2Fapi" \
-            "%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fzlapp.fudan.edu.cn%252Fsite%252Fncov%252FfudanDaily" \
-            "%253Ffrom%253Dhistory%26from%3Dwap "
+login_url = "https://uis.fudan.edu.cn/authserver/login?service=https%3A%2F%2Fzlapp.fudan.edu.cn%2Fa_fudanzlapp%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fzlapp.fudan.edu.cn%252Fsite%252Fncov%252FfudanDaily%253Ffrom%253Dhistory%26from%3Dwap"
 get_info_url = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info"
 save_url = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/save"
 code_url = "https://zlapp.fudan.edu.cn/backend/default/code"
@@ -33,13 +27,16 @@ error_string = "您将登录的是："
 max_retry = 3
 
 
-def tick_for_id(id: int):
+def tick(user, pwd, max_time):
+    print("Ticking for user ", user)
     # Randomize time
-    time.sleep(random.randint(0, 36000))
+    wait_time = random.randint(0, max_time)
+    print("Randomize time: waiting for ", wait_time)
+    time.sleep(wait_time)
 
     data = {
-        "username": usernames[id],
-        "password": passwords[id]
+        "username": user,
+        "password": pwd
     }
 
     s = requests.Session()
@@ -59,9 +56,16 @@ def tick_for_id(id: int):
         response = s.get(get_info_url)
         old_pafd_data = json.loads(response.text)
         pafd_data = old_pafd_data["d"]["info"]
+
+        ocr_detector = easyocr.Reader(['en'], gpu=False, recognizer=False)
         code = s.get(code_url).content
-        code_result = reader.readtext(code)[0][1].replace(" ", "")
-        print("Verification code result: ", code_result, "confidence: ", reader.readtext(code)[0][2])
+        boundary_a, boundary_b = ocr_detector.detect(code, reformat=True)
+        del ocr_detector
+        ocr_recognizer = easyocr.Reader(['en'], gpu=False, detector=False)
+        code_result = ocr_recognizer.recognize(code, boundary_a[0], boundary_b[0], reformat=True)
+        code_result_trimmed = code_result[0][1].replace(' ', '')
+        del ocr_recognizer
+        print("Verification code result: ", code_result_trimmed, "confidence: ", code_result[0][2])
 
         pafd_data.update({
             "ismoved": 0,
@@ -80,7 +84,7 @@ def tick_for_id(id: int):
             "jcjgqr": 0,
             "sfwztl": 0,
             "sftztl": 0,
-            "code": code_result
+            "code": code_result_trimmed
         })
 
         if not pafd_data['sfzx']:
@@ -95,7 +99,8 @@ def tick_for_id(id: int):
 
         if json.loads(response.text)['e'] == 0:
             break
-
+        if json.loads(response.text)['m'] == '今天已经填报了':
+            break
         sleep_t = random.randint(2, 10)
         print("Tick failed, retrying in ", sleep_t)
         time.sleep(sleep_t)
@@ -105,6 +110,6 @@ def tick_for_id(id: int):
         raise Exception("Tick failed")
 
 
-if __name__ == "__main__":
-    for i in range(len(usernames)):
-        Thread(target=tick_for_id, args=(i,)).start()
+if __name__ == '__main__':
+    for user in USERS:
+        Thread(target=tick, args=(user[0], user[1], user[2])).start()
